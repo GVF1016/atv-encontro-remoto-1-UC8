c83263bae2db4c69c2093a6c6434106c14285120
GVF <guilhermevale109@gmail.com>
2022-07-14 17:46:08 -0300

    salvando os arquivos da atividade

---
+++ atvPecas.js
@@ -0,0 +1,23 @@
+let peso = 150
+let listaQtdPecas = 11
+let nome = "pc"
+
+if (peso >= 100) {
+    console.log("peso aceitavel para cadastro")
+} else {
+    console.log("peso insuficiente para cadastro")
+}
+
+if (listaQtdPecas = 10) {
+    console.log("caixa em capacidade maxima")
+} else if (listaQtdPecas > 10) {
+    console.log("quantidade excedeida")
+} else {
+    console.log("caixa com espaço sobrando")
+}
+
+if ( nome.length < 3) {
+    console.log("nome invalido: minimo de 3 caracteres")
+} else {
+    console.log("nome valido")
+}
